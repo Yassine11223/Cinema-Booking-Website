65809efcae1b07a44d49de5f5c8dd3ee8334f4f8
yassine <yassine2402597@miuegypt.edu.eg>
2026-06-12 21:42:19 +0300

    Clean duplicate backend and temporary files

--- search_unsplash.ps1
+++
@@ -1,5 +0,0 @@
-$json = Invoke-RestMethod -Uri 'https://unsplash.com/napi/search/photos?query=cinema+theater+modern'
-Write-Host "Results:"
-foreach ($item in $json.results | Select-Object -First 10) {
-    Write-Host ("ID: " + $item.id)
-}
