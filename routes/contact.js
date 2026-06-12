1131d2eb65c580be12c0b929b377c3dfb6d77881
yassine <yassine2402597@miuegypt.edu.eg>
2026-06-12 23:01:57 +0300

    Remove duplicate root backend files

--- routes/contact.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { sendContactMessage } = require('../controllers/contactController');
-
-router.post('/', sendContactMessage);
-
-module.exports = router;
